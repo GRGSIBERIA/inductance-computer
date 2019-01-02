607688b0895f9cf0c0ee0fd3f3beafc5bc95eb1b
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-03 02:15:07 +0900

    エラーだらけでわからん

--- Rust/csv-migration/src/main.rs
+++ Rust/csv-migration/src/main.rs
@@ -1,3 +1,17 @@
+use std::str::FromStr;
+use std::fmt::Debug;
+
+fn read_line<T>() -> Vec<T>
+where T: FromStr, <T as FromStr>::Err : Debug{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim().split_whitespace().map(|c| T::from_str(c).unwrap()).collect()
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    
 }
