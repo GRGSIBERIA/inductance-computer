203a2a1a48ae6b9d6b1881f9f398e77f8d59cef7
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-04 00:15:19 +0900

    わからん

--- Rust/csv-migration/src/main.rs
+++ Rust/csv-migration/src/main.rs
@@ -2,14 +2,27 @@ use std::str::FromStr;
 use std::fmt::Debug;
 
 fn read_line<T>() -> Vec<T>
-where T: FromStr, <T as FromStr>::Err : Debug{
+where T: FromStr, <T as FromStr>::Err : Debug {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
     s.trim().split_whitespace().map(|c| T::from_str(c).unwrap()).collect()
 }
 
 fn main() {
-    let x : Vec<u32> = read_line();
-    let num = x[1];
+    
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    let num: i32 = s.parse().unwrap();
+
     println!("{}", num);
+
+    println!("Number of times:");
+
+    println!("Number of time division:");
+
+    println!("Coil's height:");
+
+    println!("Wire's position of top of the coil:");
+
+    println!("Range of horizontal movement of wire:");
 }
