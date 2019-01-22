c812c21af5af3eb1316b3284f169810b3ba3a097
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-22 21:49:10 +0900

    簡単な動作テストがやっとできるようになった

--- InductanceComputer/AleaTestProject/Program.cs
+++ InductanceComputer/AleaTestProject/Program.cs
@@ -3,16 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using Alea;
 using Alea.Parallel;
+
 
 namespace AleaTestProject
 {
     class Program
     {
+        static void RunParallel()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var result = AreaCore.RunParallel();
+            watch.Stop();
+
+            Console.WriteLine($"RunParallel: {watch.ElapsedMilliseconds} ms");
+
+            for (int i = 0; i < result.Length; ++i)
+            {
+                Debug.Assert((double)i == result[i]);
+            }
+        }
+
+        static void RunSquare()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var result = AreaCore.RunSquare();
+            watch.Stop();
+
+            Console.WriteLine($"RunSquare: {watch.ElapsedMilliseconds} ms");
+
+            for (int i = 0; i < result.Length; ++i)
+            {
+                Debug.Assert((double)i * (double)i == result[i]);
+            }
+        }
+
         static void Main(string[] args)
         {
-            AreaCore.RunParallel();
+            RunParallel();
+            RunSquare();
         }
     }
 
@@ -25,6 +59,18 @@ namespace AleaTestProject
             Gpu.Default.For(0, result.Length, n =>
             {
                 result[n] = (double)n;
+            });
+
+            return result;
+        }
+
+        public static double[] RunSquare()
+        {
+            double[] result = new double[100 * 100 * 100];
+
+            Gpu.Default.For(0, result.Length, n =>
+            {
+                result[n] = (double)n * (double)n;
             });
 
             return result;
