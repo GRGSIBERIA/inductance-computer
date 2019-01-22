71adab22bb307eb185e86567369664139c81ecc9
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-22 22:24:39 +0900

    自作型のテスト

--- InductanceComputer/AleaTestProject/Program.cs
+++ InductanceComputer/AleaTestProject/Program.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using Alea;
 using Alea.Parallel;
 using CudaComputing;
-
+using System.Collections;
 
 namespace AleaTestProject
 {
@@ -58,11 +58,27 @@ namespace AleaTestProject
             }
         }
 
+        static void RunDouble3()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var result = AreaCore.RunDouble3();
+            watch.Stop();
+
+            Console.WriteLine($"RunDouble3: {watch.ElapsedMilliseconds} ms");
+
+            for (int i = 0; i < result.Length; ++i)
+            {
+                Debug.Assert((double)i == result[i].x);
+            }
+        }
+
         static void Main(string[] args)
         {
             RunParallel();
             RunSquare();
             RunVector();
+            RunDouble3();
         }
     }
 
@@ -104,6 +120,55 @@ namespace AleaTestProject
             });
 
             return result;
+        }
+
+        public static double3[] RunDouble3()
+        {
+            double3[] result = new double3[100 * 100 * 100];
+
+            Gpu.Default.For(0, result.Length, n =>
+            {
+                result[n] = new double3();
+                result[n].x = (double)n;
+            });
+
+            return result;
+        }
+    }
+
+    public struct double3 : IEquatable<double3>, IStructuralEquatable, IComparable<double3>, IComparable, IStructuralComparable
+    {
+        public double x, y, z;
+
+        public int CompareTo(object other, IComparer comparer)
+        {
+            return (object)this == other ? 1 : 0;
+        }
+
+        public int CompareTo(double3 other)
+        {
+            return other.x == x && other.y == y && other.z == z ? 1 : 0;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return (object)this == obj ? 1 : 0;
+        }
+
+        public bool Equals(object other, IEqualityComparer comparer)
+        {
+            var obj = (double3)other;
+            return x == obj.x && y == obj.y && z == obj.z;
+        }
+
+        public bool Equals(double3 other)
+        {
+            return other.x == x && other.y == y && other.z == z;
+        }
+
+        public int GetHashCode(IEqualityComparer comparer)
+        {
+            return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
         }
     }
 }
