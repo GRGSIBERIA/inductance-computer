cb83d38ba15121a859e6161ca49efcefe567bc6b
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-01-22 17:17:07 +0900

    プロジェクトの立ち上げ

---
+++ InductanceComputer/CudaComputing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CudaComputing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
