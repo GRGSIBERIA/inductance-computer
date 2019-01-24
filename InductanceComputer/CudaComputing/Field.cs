e3afa72c4c5479f55eb421abc685c180087feb0f
grgsiberia <nanashi4129@gmail.com>
2019-01-24 17:23:04 +0900

    Fieldを作成しておく

---
+++ InductanceComputer/CudaComputing/Field.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CudaComputing
+{
+    public class Field
+    {
+        public double Inductance { get; private set; }
+
+        public Vector3 Position { get; private set; }
+        public Vector3 Size { get; private set; }
+
+        public Field(Vector3 position, double width, double height, double depth)
+        {
+            Size = new Vector3(width, height, depth);
+            Position = position;
+        }
+    }
+}
