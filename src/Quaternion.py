7fd812f6304822df7d41e6e1f5fdd1bbf960ad5c
grgsiberia <nanashi4129@gmail.com>
2019-01-28 19:19:05 +0900

    numbaの警告が頻発する

--- src/Quaternion.py
+++ src/Quaternion.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numba import jit
 
 class Quaternion:
+    @jit
     def __init__(self, r: float, v: np.array):
         self.v = v
         self.r = r
@@ -9,12 +11,14 @@ class Quaternion:
         self.v = -self.v
         return self
     
+    @jit
     def norm(self):
         total = self.distance()
         self.v = self.v / total
         self.r = self.r / total
         return self
     
+    @jit
     def distance(self):
         total = 0.
         for i in range(3):
@@ -25,13 +29,17 @@ class Quaternion:
     def inverse(self):
         return self.conj()
     
-    def __mul__(self, q):
-        return Quaternion(self.r * q.r - np.dot(self.v, q.v), self.r * q.v + q.r * self.v + np.cross(self.v, q.v))
+    @jit
+    def __mul__(self, q: np.array):
+        t = self.r * q.r - np.dot(self.v, q.v)
+        v = self.r * q.v + q.r * self.v + np.cross(self.v, q.v)
+        return Quaternion(t, v)
 
+    @jit
     def rotation(self, p: np.array, theta: float):
         cos = np.cos(theta * 0.5)
         sin = np.sin(theta * 0.5)
         P = Quaternion(0, p)
         Q = Quaternion(cos, self.v * sin)
-        R = Quaternion(cos, -self.v * sin)
+        R = Quaternion(cos, self.v * -sin)
         return (Q * P * R).v
--- src/Wire.py
+++ src/Wire.py
@@ -2,22 +2,22 @@ from typing import List
 import numpy as np
 from numba import jit
 from scipy.integrate import dblquad
-from Coil import Coil
-from Quaternion import Quaternion
+from src.Coil import Coil
+from src.Quaternion import Quaternion
 
 class Wire:
-    def __init__(self, time: float, positions: np.array, deltaDim:List[int]):
-        self.time =  time
+    def __init__(self, positions: np.array):
         self.positions = positions
-        self.deltaDim = deltaDim
         self.fluxDensity = 0.
         self._sigma = 1.
 
-    @jit
     def _quadCoil(self, dr: float, dt: float, coil: Coil):
         fracUp = self._sigma * dr
+
         q = Quaternion(0, coil.front)
-        fracDownVec = dr * q.rotation(dt, coil.right) + self.positions - coil.position
+        rotated = dr * q.rotation(coil.right, dt)
+
+        fracDownVec = rotated + self.positions - coil.position
         fracDownVal = np.linalg.norm(fracDownVec)
         fracDownVal = fracDownVal * fracDownVal * fracDownVal
         return fracUp / fracDownVal
@@ -27,6 +27,6 @@ class Wire:
         self._sigma = sigma
 
         for coil in coils:
-            self.fluxDensity += dblquad(self._quadCoil, 0, 2.*np.pi, lambda x: 0, lambda x: coil.radius, (coil))[0]
+            self.fluxDensity += dblquad(self._quadCoil, 0, 2.*np.pi, lambda x: 0, lambda x: coil.radius, args=[coil])[0]
 
         return self.fluxDensity
